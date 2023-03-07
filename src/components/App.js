07154dacecf708a41cf21bbc0c28250c66e781f8
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-07 01:27:45 -0300

    :sparkles: create routes for all paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import GlobalStyle from "./styles/GlobalStyle";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { GlobalContext } from "../contexts/GlobalContext";
+import { UserContext } from "../contexts/UserContext";
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <h1>Hello Linkr!</h1>
-    </>
+    <BrowserRouter>
+      <GlobalContext.Provider value={{ nothing: null }}>
+        <UserContext.Provider value={{ user: null }}>
+          <Routes>
+            {/* <Route path="/" element={<Login />} /> */}
+            {/* <Route path="/sign-up" element={<Signup />} /> */}
+            {/* <Route path="/hashtag/:hashtag" element={<Hashtag />} /> */}
+            {/* <Route path="/timeline" element={<Timeline />} /> */}
+            {/* <Route path="/user/:id" element={<User />} /> */}
+          </Routes>
+        </UserContext.Provider>
+      </GlobalContext.Provider>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./components/App";
+import GlobalStyle from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
